3dfeda5898b2e85a55e5e4915a0f21ebfc103692
Ứng Nguyễn <ungnguyen@Ungs-MacBook-Air.local>
2023-08-01 18:36:38 +0700

    Resource controller with dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProductsModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
